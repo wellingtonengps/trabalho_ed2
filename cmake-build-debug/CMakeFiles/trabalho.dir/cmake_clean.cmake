d16111059a1c5f8e549043596a1e86830e6eea0d
Wellington Pereira <43380060+wellingtonengps@users.noreply.github.com>
2021-11-02 17:04:24 -0300

    projeto inicial do clion

---
+++ cmake-build-debug/CMakeFiles/trabalho.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trabalho.dir/main.cpp.obj"
+  "libtrabalho.dll.a"
+  "trabalho.exe"
+  "trabalho.exe.manifest"
+  "trabalho.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trabalho.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, MUNDO!" << std::endl;
+    return 0;
+}
