6f2407d7227ec54e91ac1cc269ccf52988af2952
Wellington Pereira <43380060+wellingtonengps@users.noreply.github.com>
2021-11-06 15:41:25 -0300

    leitura

--- main.cpp
+++ main.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include "Review.h"
 
 using namespace std;
 
 void read(string path){
-    
+    ifstream inputFile;
+    string linha;
+    inputFile.open(path, ios::in);
+    Review* review;
+
+    if(!inputFile){
+        return ;
+    }
+
+    getline(inputFile, linha);
+    while(!inputFile.eof()){
+        getline(inputFile, linha);
+        review =readLine(linha);
+        cout << linha << endl;
+        delete review;
+    }
+
+    inputFile.close();
+
 }
 
 int main(int argc, char ** argv) {
@@ -19,6 +38,8 @@ int main(int argc, char ** argv) {
         cout << "Erro: era esperado caminho do arquivo de entrada."<<endl;
         return 0;
     }
+
+    read("../data.csv");
 
     return 0;
 }
