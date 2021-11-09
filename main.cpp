dea155a36ef07b898c085fb77f0523e6c36cd845
Wellington Pereira <43380060+wellingtonengps@users.noreply.github.com>
2021-11-08 21:50:40 -0300

    leitura versao caracters

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
 #include "Review.h"
 
 using namespace std;
@@ -47,6 +49,79 @@ Review* readLine(string line){
 
 }
 
+void conferir(string path){
+    ifstream inputFile;
+    inputFile.open(path, ios::in);
+    string linha;
+    while(!inputFile.eof()){
+        getline(inputFile, linha);
+
+        //cout << linha << endl;
+        if(linha[0]!='g' && linha[1]!='p' && linha[2]!=':'){
+            cout <<"Merda!"<<endl;
+            cout<<linha<<endl;
+        }
+
+    }
+}
+
+
+void readCaracteres(string path){
+
+    FILE *arq;
+    char c;
+
+    arq = fopen("../data.csv", "r");
+
+    if(arq != NULL){
+         bool ignoraVirgula = false;
+         int campoAtual =0;
+         string value;
+
+        while((c = fgetc(arq)) != EOF ){
+            cout << c;
+
+        if(c=='\"'){
+            ignoraVirgula=!ignoraVirgula;
+        }
+        else if(c==',' && ignoraVirgula==false){
+            cout << '\n';
+
+            if(campoAtual==0){
+               // review->setReviewId(value);
+               cout << value << " - ";
+            }else if(campoAtual==1){
+               // review->setReviewText(value);
+                cout << value << " - ";
+            } else if(campoAtual==2){
+              //  review->setUpvotes(stoi(value));
+                cout << value << " - ";
+            } else if(campoAtual==3){
+              //  review->setAppVersion(value);
+                cout << value << " - ";
+            } else if(campoAtual==4){
+               // review->setPostedDate(value);
+                cout << value << " - ";
+
+            }
+            value.clear();
+            campoAtual++;
+        }else{
+            //cout<< c;
+            value+=c;
+            campoAtual=0;
+            value.clear();
+        }
+    }
+        fclose(arq);
+    }
+    else{
+        printf("Error ao abrir o arquivo");
+    }
+
+
+}
+
 void read(string path){
     ifstream inputFile;
     string linha;
@@ -56,6 +131,7 @@ void read(string path){
     if(!inputFile){
         return ;
     }
+
 
     getline(inputFile, linha);
     while(!inputFile.eof()){
@@ -75,6 +151,12 @@ int main(int argc, char ** argv) {
     std::cout << "Trabalho ED2" << std::endl;
     string input_file;
 
+    //conferir("../tiktok_app_reviews.csv");
+
+    readCaracteres("../data.csv");
+
+    return 0;
+
     if(argc == 2) {
         input_file = argv[1];
 
@@ -84,6 +166,7 @@ int main(int argc, char ** argv) {
     }
 
     read("../data.csv");
+
 
     return 0;
 }
